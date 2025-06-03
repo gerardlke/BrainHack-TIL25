abbba55ee7ae4ca4771ac1db39b97ea2ba146d83
ben <ebn@ben.ben>
2025-06-03 09:24:00 +0000

    more minor changes to bring home

--- self_play_env.py
+++ self_play_env.py
@@ -61,6 +61,8 @@ from pettingzoo.utils.wrappers import BaseWrapper
 from supersuit.generic_wrappers.utils.base_modifier import BaseModifier
 from supersuit.generic_wrappers.utils.shared_wrapper_util import shared_wrapper
 
+from otherppos import ModifiedMaskedPPO, ModifiedPPO
+
 
 def build_env(
     reward_names,
@@ -214,15 +216,8 @@ class SelfPlayWrapper(aec_to_parallel_wrapper):
         self.db.set_up_db(timeout=100)
         # print('env', self.hash, 'connected to db')
         checkpoints = [
-            self.db.get_checkpoint_by_policy(policy) for policy in self.environment_policies
+            self.db.get_checkpoint_by_policy(policy, shuffle=True) for policy in self.environment_policies
         ]
-        # # self.db.export_database_to_sql_dump(self.db_path, self.db_path)
-        # print('self.db_path', self.db_path)
-        print('checkpoints', checkpoints)
-        test = self.db.get_checkpoint_by_policy(0)
-        print('other checkpoints', test)
-        for row in test:
-            print('row', dict(row))
 
         self.db.shut_down_db()
         
@@ -230,12 +225,21 @@ class SelfPlayWrapper(aec_to_parallel_wrapper):
             policy: self.load_policy(c) for c, policy in zip(checkpoints, self.environment_policies)
         }
 
-    def load_policy(self, paths: list):
-        if len(paths) == 0:
+    def load_policy(self, checkpoints: list):
+        if len(checkpoints) == 0:
             # if no checkpoint, default to random behaviour.
             return 'random'
         else:
-            return 'example_policy_here'
+            # just select the first for now
+            checkpoint = checkpoints[0]
+            hyperparams = checkpoint['hyperparameters']
+            filepath = checkpoint['filepath']
+            algo_type = eval(hyperparams['algorithm'])
+            hyperparams.pop('algorithm')
+            algo_type.load(
+                path=
+            )
+
 
     def reset(self, seed: int | None=None, options: dict | None=None):  # type: ignore
         """
